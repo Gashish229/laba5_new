0b93ea377033daa00a122783fcf78dd79a0480a3
Gashish229 <SINKOV07@BK.RU>
2025-11-26 23:32:19 +0700

    Create 16.c

---
+++ 16.c
@@ -0,0 +1,14 @@
+ #include <stdio.h>
+ int main(void)
+ {
+ float dafna = 100.0;
+ float deidra = 100.0;
+ int year;
+ for (year = 0; dafna >= deidra; year++)
+        {
+         dafna += 10;
+         deidra += (deisra * 5) / 100.0;
+        }
+printf("года: %2d | Дафна: %.1f | Дейдра: %.1f\n", year, dafna, deidra);
+ return 0;
+ }
