9607646cd709dcd3a164c39c37ef63f7e11328ce
Gashish229 <SINKOV07@BK.RU>
2025-11-26 23:38:33 +0700

    Create 18.c

---
+++ 18.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define chisel_Dunbara 150
+ int main(void)
+ {
+ int friends = 5;
+ int week;
+ for (week = 1; (friend - week) * 2 < chisel_Dunbara; week++)
+        {
+         friends = (friends - week) * 2;
+        }
+ printf("На %d неделе у профессора Робинсона будет %d друзей", week, friends);
+ return 0;
+ }
