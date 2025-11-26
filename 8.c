6a20b2dbbd64af28371302feceeea95e271d0d72
Gashish229 <SINKOV07@BK.RU>
2025-11-26 23:15:13 +0700

    Create 8.c

---
+++ 8.c
@@ -0,0 +1,12 @@
+ #include <stdio.h>
+ int main()
+ {
+   float chisel1, chisel2;
+   printf("Введите два числа с плавающей запятой:\n");
+   while (scanf("%f %f" , &chisel1, &chisel2))
+   {
+          printf("Результат: %f\n", ((chislo1 - chislo2) / (chislo1 * chislo2)));
+          printf("Введите новые числа (Нечисловое значения для выхода):\n");
+   }
+   return 0;
+ }
