f8202082a10df04ed56fc39a12a5b2f24826ca64
Gashish229 <SINKOV07@BK.RU>
2025-11-26 23:29:14 +0700

    Update 14.c

--- 14.c
+++ 14.c
@@ -8,22 +8,22 @@ int main(void)
    printf("введите 8 чисел:\n");
    for (i = 0; i < 8; i++)
    {
-      while (scanf("%d", &mas[i]) != 1)
+      while (scanf("%d", &mas1[i]) != 1)
       {
          printf("!Ошибка ввода!\n");
          while ((ch = getchar()) != '\n' && ch != EOF);
       }
-      sum += mas[i];
-      mas[i] = sum;
+      sum += mas1[i];
+      mas2[i] = sum;
    }
    for (i = 0; i < 8; i++)
    {
-      printf("%5d", mas[i]);
+      printf("%5d", mas1[i]);
    }
    printf("\n");
    for (i = 0; i < 8; i++)
    {
-      printf("%5d", mas[i]);
+      printf("%5d", mas2[i]);
    }
    return 0;
 }
